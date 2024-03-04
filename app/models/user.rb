ae903f942939b306c3b9ba1ebc32b934caa46d1c
Shoki-darou <shoki.rugby0115@gmail.com>
2024-03-04 18:13:19 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   #has_many :items
 
   validates :name,            presence: true
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,       message: "は全角（カタカナ）で入力してください" }
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/,       message: "は全角（カタカナ）で入力してください" }
   validates :birthday,        presence: true
