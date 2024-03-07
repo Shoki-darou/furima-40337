16043a2855d674783139117ddb3241df660446ba
Shoki-darou <shoki.rugby0115@gmail.com>
2024-03-07 20:25:48 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,34 +20,36 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-
   end
-=begin
+
   def edit
     @item = Item.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: :index
+    if current_user == @item.user && @item.sold_out?
+      redirect_to root_path
+    elsif current_user != @item.user
+      redirect_to root_path
     end
   end
   
   def update
-    item = Item.find(params[:id])
-    item.update(prototype_params)
-    if item.save
-      redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
-
+=begin
   def destroy
     prototype = Item.find(params[:id])
     prototype.destroy
     redirect_to '/'
   end
 =end
+
   private
-    def item_params
-      params.require(:item).permit(:item_name, :content, :price, :category_id, :delivery_id, :city_id, :days_to_ship_id, :status_id, :image).merge(user_id: current_user.id)
-    end
+  def item_params
+    params.require(:item).permit(:item_name, :content, :price, :category_id, :delivery_id, :city_id, :days_to_ship_id, :status_id, :image).merge(user_id: current_user.id)
+  end
+
 end
