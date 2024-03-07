a343656dd7a02e490878ad2d59e57556a0937083
Shoki-darou <shoki.rugby0115@gmail.com>
2024-03-07 21:08:19 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item
   end
 
   def edit
@@ -40,8 +40,8 @@ class ItemsController < ApplicationController
   end
 =begin
   def destroy
-    prototype = Item.find(params[:id])
-    prototype.destroy
+    @item = Item.find(params[:id])
+    @item.destroy
     redirect_to '/'
   end
 =end
