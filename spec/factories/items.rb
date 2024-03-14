1cf8b42804f0124909c9e3c97bc68811a7045310
Shoki-darou <shoki.rugby0115@gmail.com>
2024-03-14 15:12:47 +0900

    不具合の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,8 +10,8 @@ FactoryBot.define do
     price           { Faker::Number.within(range: 300..9_999_999) }
     association :user
 
-    after(:build) do |message|
-      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    after(:build) do |item|
+      item.image.attach(io: File.open(Rails.root.join('public', 'images', 'test_image.png')), filename: 'test_image.png', content_type: 'image/png')
     end
   end
 end
